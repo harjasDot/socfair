01a239858ec868070c6d126115557e6d42a013ec
Harjaspreet singh <singh.harjas2002@gmail.com>
2022-09-17 15:55:47 +0530

    ff

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ class QUESTION(models.Model):
     name=models.CharField(blank=False,max_length=50)
     q1=models.CharField(blank=False,max_length=900)
     q2=models.CharField(blank=False,max_length=900)
+    q3=models.CharField(blank=False,max_length=900)
 
 
     def __str__(self):
--- app/views.py
+++ app/views.py
@@ -7,10 +7,12 @@ def core(request):
         name=request.POST['name']
         q1=request.POST['q1']
         q2=request.POST['q2']
+        q3=request.POST['q3']
         data=QUESTION.objects.create(
         name=name,
         q1=q1,
         q2=q2,
+        q3=q3
         )
         data.save()
 
